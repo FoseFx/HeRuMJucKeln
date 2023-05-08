acf777e32dbcb6d5863d7ed0370ab9ef04897585
Max Baumann <maximilian.baumann@rwth-aachen.de>
2023-05-08 18:30:48 +0200

    Resolve "Scale indicator (Map)"
    Closes #14
    
    See merge request swc/teaching/summer-term-2023/daten-die-uns-bewegen/herumjuckeln/herumjuckeln!21

--- components/RawMap.vue
+++ components/RawMap.vue
@@ -5,6 +5,7 @@
       :map-style="mapStyle"
       :center="center"
       :zoom="zoom"
+      @mb-created="onMapCreated"
     >
       <!-- TODO: Place markers according to busses
         All properties but position can be left out
@@ -30,6 +31,7 @@
 
 <script setup lang="ts">
 import { MapboxMap, MapboxNavigationControl } from "@studiometa/vue-mapbox-gl";
+import { Map, ScaleControl } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const config = useRuntimeConfig();
@@ -41,6 +43,19 @@ const mapStyle = computed(
 
 const center = [6.07998, 50.77791]; // TODO: calculate using data
 const zoom = 10; // TODO: calculate using data
+
+// Adds an indicator that gives the user a sense for the scale of the map.
+// I tries to keep the number on the scale simple and changes in width acordingly
+// Related Documentation:
+// - https://docs.mapbox.com/mapbox-gl-js/api/markers/#scalecontrol
+function addScaleIndicator(map: Map) {
+  const scale = new ScaleControl();
+  map.addControl(scale);
+}
+
+function onMapCreated(map: Map) {
+  addScaleIndicator(map);
+}
 </script>
 
 <style>
