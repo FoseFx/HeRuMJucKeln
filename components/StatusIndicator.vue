d60151406d3ca8bc3806e366c40fef588513e274
Max Baumann <maximilian.baumann@rwth-aachen.de>
2023-07-03 17:31:51 +0200

    Resolve "Certificate check"
    Closes #76
    
    See merge request swc/teaching/summer-term-2023/daten-die-uns-bewegen/herumjuckeln/herumjuckeln!92

--- components/StatusIndicator.vue
+++ components/StatusIndicator.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="status-indicator">
-    <div v-if="!itineratiesLoaded" class="warning">
+    <div v-if="connectionError" class="error">
+      <VIcon icon="mdi-alert" color="--color-alert" />
+      Verbindung fehlgeschlagen. Zertifikat installiert?
+    </div>
+    <div v-else-if="!itineratiesLoaded" class="warning">
       <VIcon icon="mdi-alert" color="orange" />
       Stopps werden noch geladen
     </div>
@@ -9,10 +13,12 @@
 </template>
 
 <script setup lang="ts">
+const { isRejected: connectionError, isResolved: connectionOk } = useLines();
+
 const itineraties = useItinerariesForAllVehicles();
 const itineratiesLoaded = computed(() => itineraties.value.size !== 0);
 
-const ready = computed(() => itineratiesLoaded.value);
+const ready = computed(() => connectionOk.value && itineratiesLoaded.value);
 
 // short flash of "ok we done now"
 const showReady = refAutoReset(false, 5000);
@@ -24,6 +30,10 @@ watch(ready, (isReady) => isReady && (showReady.value = true));
   padding-left: 1rem;
   padding-right: 1rem;
   font-size: 0.85rem;
+}
+.error {
+  color: var(--color-alert);
+  font-weight: bold;
 }
 .warning {
   color: var(--color-warning);
